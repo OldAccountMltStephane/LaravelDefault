328c97020db66c77182c447afe45da6226d69878
StephDw <spmulot@gmail.com>
2020-03-02 22:57:42 +0100

    Initial commit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 php artisan down
+
 cp .env.example .env
 php artisan key:generate
+
 touch database/database.sqlite
 php artisan migrate:fresh
+
+composer require
+npm install
+npm run dev
+
 php artisan up
